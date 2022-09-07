df25fbc96a9e50cd1c0c81cd64b62c4c1690e0dd
Bhavadharani <bhavadharan.S@fundsindia.com>
2022-09-07 17:45:49 +0530

    added playground file

---
+++ Variabless.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+var greeting = "Hello, playground"
+greeting = "Hello user"
+print("Check:\(greeting)")
+
+
+//var, let - difference
+//Datatypes
+//Access identifiers
+            
+//variables
+var variable = 42
+print(variable)
+
+//
+var stringName="beauty"
+print(stringName)
+
+//type annotations
+
+var varA="apple"
+print(varA)
+
+var varB:Float
+
+varB=43.567
+print(varB)
+
+// writing inside the print command
+var var1="cat"
+var var2="dog"
+print("\(var1)is bigger than \(var2)")
+
+//typealiases
+ 
+typealias New = Int
+var distance: New = 100
+print(distance)
+
+//
+let  num = 100
+    
