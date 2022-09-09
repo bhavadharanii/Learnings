4e5e83b4b049ed46952deb7fc0b374a0e3fae811
Bhavadharani <bhavadharan.S@fundsindia.com>
2022-09-09 10:00:01 +0530

    Updated

---
+++ Variabless.playground/Pages/operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,55 @@
+// operators
+
+print(5+6)
+print(5*6)
+var a=5
+var b=856
+print(a+b,a*b,a%b,a-b)
+print(a/b)
+var c = 5.0
+
+var d = 10.0
+print(d/c)
+
+//assignment operator
+var f = 50
+var e = 50
+f%=e
+print(f)
+ print(f>e)
+print(f<e)
+print((f<e)||(e>f))
+print((f==e)||(e==f))
+
+// ternary operator
+print((f<e) ? "sucess":"failure")
+for numbers in 1...34
+{
+        print(numbers)
+}
+
+//stringinterpolation
+var cityName = "mumbai"
+print("I live in \(cityName)")
+
+var  name = "bhavadharani"
+var age = 21
+print (" my name is \(name) and my age is \(age)")
+
+//functions
+
+func greet()
+{
+    print("helloworld")
+}
+//for printing we have to call tha functioon that we have created(greet)
+greet()
+print("function is called")
+
+func addNumbers(var1: Int, var2: Int)
+{
+    let sum = var1 + var2
+    print("sum: ",sum)
+}
+func addNumbers(var1: 2, var2: 6)
+
