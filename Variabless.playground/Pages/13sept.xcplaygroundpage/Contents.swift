fbb80eddfac9c969ca91710f65a740146f436520
Bhavadharani <bhavadharan.S@fundsindia.com>
2022-09-13 16:58:53 +0530

    Changes Updates as of now

--- Variabless.playground/Pages/13sept.xcplaygroundpage/Contents.swift
+++ Variabless.playground/Pages/13sept.xcplaygroundpage/Contents.swift
@@ -11,5 +11,15 @@ print(letter)
 var letter1 : String = "tharani"
 print(letter1)
 
+var letter2 : String = "tharani"
+print(letter2)
+
+var letter3 : String = "tharani"
+print(letter3)
+
+var letter4 : String = "tharani"
+print(letter4)
+
+
 
 //
