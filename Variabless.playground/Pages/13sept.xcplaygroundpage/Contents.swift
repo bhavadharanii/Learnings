a7e11c99ad80da95009d130003b014b3124f0708
Bhavadharani <bhavadharan.S@fundsindia.com>
2022-09-13 17:14:18 +0530

    updated

--- Variabless.playground/Pages/13sept.xcplaygroundpage/Contents.swift
+++ Variabless.playground/Pages/13sept.xcplaygroundpage/Contents.swift
@@ -4,20 +4,21 @@ let checkF: Bool = false
 print(checkF)
 
     //character
-var letter:Character = ”s”
+
+var letter:Character =  ”s”
 print(letter)
 
 //string
-var letter1 : String = "tharani"
+var letter1 : String = "swift"
 print(letter1)
 
-var letter2 : String = "tharani"
+var letter2 : String = "functioning"
 print(letter2)
 
-var letter3 : String = "tharani"
+var letter3 : String = "except"
 print(letter3)
 
-var letter4 : String = "tharani"
+var letter4 : String = "changes"
 print(letter4)
 
 
