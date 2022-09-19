c0aa4be0e34d3e74248d252897ef9d9ed7726344
Bhavadharani <bhavadharan.S@fundsindia.com>
2022-09-19 11:27:37 +0530

    updated

--- Basics-1.playground/Pages/classess.xcplaygroundpage/Contents.swift
+++ Basics-1.playground/Pages/classess.xcplaygroundpage/Contents.swift
@@ -4,29 +4,31 @@ class student
     var studentId : Int
     var studentMark : Int
     
-    
     init(studentName : String , studentId : Int ,studentMark : Int)
     {
         self.studentName = studentName
         self.studentId = studentId
         self.studentMark = studentMark
-        
-        
     }
 }
 class studentMarks{
-    
     var studentName =  "bhavadharani"
-    var studentId = 234
+    var studentId = 2345
     var studentMark = 90
 }
-let studentName = studentMarks()
-let studentId = studentMarks()
-let studentMark = studentMarks()
+var studentDetaiils = studentMarks()
+var detailsCopy = studentDetaiils
+print("Before update studentDetaiils: \(studentDetaiils.studentName), \(studentDetaiils.studentId), \(studentDetaiils.studentMark)")
+print("Before update detailsCopy: \(detailsCopy.studentName), \(detailsCopy.studentId), \(detailsCopy.studentMark)")
+studentDetaiils.studentName = "Dharani"
+print("After update studentDetaiils: \(studentDetaiils.studentName), \(studentDetaiils.studentId), \(studentDetaiils.studentMark)")
+print("After update detailsCopy: \(detailsCopy.studentName), \(detailsCopy.studentId), \(detailsCopy.studentMark)")
+//let studentId = studentMarks()
+//let studentMark = studentMarks()
 
-print("The name of the is :\(studentName.studentName)")
-print("The Id number of the student is : \(studentId.studentId)")
-print("The mark of the student is : \(studentMark.studentMark)")
+//print("The name of the is :\(studentDetaiils.studentName)")
+//print("The Id number of the student is : \(studentDetaiils.studentId)")
+//print("The mark of the student is : \(studentDetaiils.studentMark)")
 
 //assigining the values too another variable
 
@@ -41,7 +43,7 @@ print("The mark of the student is : \(studentMark.studentMark)")
 //    }
 //}
 //func(create : sampleClass,create1 : sampleClass)-> Bool{
-//    retturn
+//    return
 //}
 //class procedure
 //{
@@ -57,37 +59,34 @@ print("The mark of the student is : \(studentMark.studentMark)")
 //            self.procedureId = procedureId
 //        }
 //}
- 
-//Inhertence
-
-class StudDetails {
-   var mark1: Int;
-   var mark2: Int;
-   //initializing
-   init(stm1:Int, results stm2:Int) {
-      mark1 = stm1;
-      mark2 = stm2;
-   }
-    //function
-   func function() {
-      print("Mark1:\(mark1), Mark2:\(mark2)")
-   }
-}
-//ubclass 'display' is used to inherit the marks from its super class.
-class display : StudDetails {
-   init() {
-       //superclass
-      super.init(stm1: 93, results: 89)
-   }
-}
-//calliing the instance form the class display->StudentDetails
-let marksobtained = display()
-//marksobtaiined->display->stuentDetails->function(marks and mark2
-
-marksobtained.function()
-
-
-
+//
+////Inhertence
+//
+//class StudDetails {
+//   var mark1: Int;
+//   var mark2: Int;
+//   //initializing
+//   init(stm1:Int, results stm2:Int) {
+//      mark1 = stm1;
+//      mark2 = stm2;
+//   }
+//    //function
+//   func function() {
+//      print("Mark1:\(mark1), Mark2:\(mark2)")
+//   }
+////}
+////ubclass 'display' is used to inherit the marks from its super class.
+//class display : StudDetails {
+//   init() {
+//       //superclass
+//      super.init(stm1: 93, results: 89)
+//   }
+//}
+////calliing the instance form the class display->StudentDetails
+//let marksobtained = display()
+////marksobtaiined->display->stuentDetails->function(marks and mark2
+//
+//marksobtained.function()
 
 //FUNCTIONINGOVERRIDING
 //superclass
@@ -103,10 +102,25 @@ class tennis : cricket {
    }
 }
 //create an object of  the subclass
-let cricinstance = cricket()
+let cricinstance: cricket = cricket()
 //call the function1 method
 cricinstance.function1()
 
 let tennisinstance = tennis()
 tennisinstance.function1()
 
+// reference type soo any changes in the variable will be called to all theall the reference
+class ArticleClass {
+    let title : String
+    var name  : String
+    var readCount: Int = 0
+
+    init(title: String, name : String) {
+        self.title = title
+        self.name = name
+    }}
+let articleClass = ArticleClass(title: "Class", name: "Bhavadharani")
+let articleClassCopy = articleClass
+
+articleClass.readCount = 10
+print(articleClassCopy.readCount) // Prints: 10
