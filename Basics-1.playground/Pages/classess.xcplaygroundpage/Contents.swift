5d7a4f33688624941f5a8362d88cd2a4ee4188af
Bhavadharani <bhavadharan.S@fundsindia.com>
2022-09-20 11:42:58 +0530

    updated

--- Basics-1.playground/Pages/classess.xcplaygroundpage/Contents.swift
+++ Basics-1.playground/Pages/classess.xcplaygroundpage/Contents.swift
@@ -1,84 +1,86 @@
-class student
-{
-    var studentName : String
-    var studentId : Int
-    var studentMark : Int
+//class student
+//{
+//    var studentName : String
+//    var studentId : Int
+//    var studentMark : Int
+//
+//    init(studentName : String , studentId : Int ,studentMark : Int)
+//    {
+//        self.studentName = studentName
+//        self.studentId = studentId
+//        self.studentMark = studentMark
+//    }
+//}
+//class studentMarks{
+//    var studentName =  "bhavadharani"
+//    var studentId = 2345
+//    var studentMark = 90
+//}
+//var studentDetaiils = studentMarks()
+//var detailsCopy = studentDetaiils
+//print("Before update studentDetaiils: \(studentDetaiils.studentName), \(studentDetaiils.studentId), \(studentDetaiils.studentMark)")
+//print("Before update detailsCopy: \(detailsCopy.studentName), \(detailsCopy.studentId), \(detailsCopy.studentMark)")
+//studentDetaiils.studentName = "Dharani"
+//print("After update studentDetaiils: \(studentDetaiils.studentName), \(studentDetaiils.studentId), \(studentDetaiils.studentMark)")
+//print("After update detailsCopy: \(detailsCopy.studentName), \(detailsCopy.studentId), \(detailsCopy.studentMark)")
+//print(" Details copy memory \(Unmanaged.passUnretained(detailsCopy).toOpaque())")
+//print("Details original memory \(Unmanaged.passUnretained(studentDetaiils).toOpaque())")
+//
 
-    init(studentName : String , studentId : Int ,studentMark : Int)
-    {
-        self.studentName = studentName
-        self.studentId = studentId
-        self.studentMark = studentMark
-    }
-}
-class studentMarks{
-    var studentName =  "bhavadharani"
-    var studentId = 2345
-    var studentMark = 90
-}
-var studentDetaiils = studentMarks()
-var detailsCopy = studentDetaiils
-print("Before update studentDetaiils: \(studentDetaiils.studentName), \(studentDetaiils.studentId), \(studentDetaiils.studentMark)")
-print("Before update detailsCopy: \(detailsCopy.studentName), \(detailsCopy.studentId), \(detailsCopy.studentMark)")
-studentDetaiils.studentName = "Dharani"
-print("After update studentDetaiils: \(studentDetaiils.studentName), \(studentDetaiils.studentId), \(studentDetaiils.studentMark)")
-print("After update detailsCopy: \(detailsCopy.studentName), \(detailsCopy.studentId), \(detailsCopy.studentMark)")
-print(" Details copy memory \(Unmanaged.passUnretained(detailsCopy).toOpaque())")
-print("Details original memory \(Unmanaged.passUnretained(studentDetaiils).toOpaque())")
-
-//FUNCTIONINGOVERRIDING
-//superclass
-class cricket {
-   func function1() {
-      print("Welcome to Swift 4 Super Class")
-   }
-}
-//subclass
-class tennis : cricket {
-   override func function1() {
-       print("Welcome to Swift 4 Sub Class")
-   }
-}
-//create an object of  the subclass
-let cricinstance: cricket = cricket()
-//call the function1 method
-cricinstance.function1()
-
-let tennisinstance = tennis()
-tennisinstance.function1()
-
-// reference type soo any changes in the variable will be called to all theall the reference
-class ArticleClass {
-    let title : String
-    var name  : String
-    var readCount: Int = 0
-
-    init(title: String, name : String) {
-        self.title = title
-        self.name = name
-    }}
-let articleClass = ArticleClass(title: "Class", name: "Bhavadharani")
-let articleClassCopy = articleClass
-
-articleClass.readCount = 10
-print(articleClassCopy.readCount) // Prints: 10
-
-
-
-class employeeDetails{
-
-    var employeeName = "Bhavadharani"
-    var employeeContact = 2345678
-    var employeeId = 23
-}
-
-    let detail = employeeDetails()
-    detail.employeeId = 25
-let newDetail = employeeDetails()
-print(newDetail.employeeName)
-print(newDetail.employeeContact)
-print(detail.employeeId)
-
+import Darwin
+////FUNCTIONINGOVERRIDING
+////superclass
+//class cricket {
+//   func function1() {
+//      print("Welcome to Swift 4 Super Class")
+//   }
+//}
+////subclass
+//class tennis : cricket {
+//   override func function1() {
+//       print("Welcome to Swift 4 Sub Class")
+//   }
+//}
+////create an object of  the subclass
+//let cricinstance: cricket = cricket()
+////call the function1 method
+//cricinstance.function1()
+//
+//let tennisinstance = tennis()
+//tennisinstance.function1()
+//
+//// reference type soo any changes in the variable will be called to all theall the reference
+//class ArticleClass {
+//    let title : String
+//    var name  : String
+//    var readCount: Int = 0
+//
+//    init(title: String, name : String) {
+//        self.title = title
+//        self.name = name
+//    }}
+//let articleClass = ArticleClass(title: "Class", name: "Bhavadharani")
+//let articleClassCopy = articleClass
+//
+//articleClass.readCount = 10
+//print(articleClassCopy.readCount) // Prints: 10
+//
+//
+//
+//class employeeDetails{
+//
+//    var employeeName = "Bhavadharani"
+//    var employeeContact = 2345678
+//    var employeeId = 23
+//}
+//
+//    let detail = employeeDetails()
+//    detail.employeeId = 25
+//let newDetail = employeeDetails()
+//print(newDetail.employeeName)
+//print(newDetail.employeeContact)
+//print(detail.employeeId)
+//
 
 
 
@@ -88,10 +90,10 @@ class newStruct
 {
     let myName : String
     let myAge : Int
-    let course : String
+    var course : String?
     var city : String
     
-    init(myName : String, myAge : Int, course : String, city : String)
+    init(myName : String, myAge : Int, course : String? = nil, city : String)
     {
     self.myName = myName
     self.myAge = myAge
@@ -99,37 +101,78 @@ class newStruct
     self.city = city
 }
 }
-let newCity = newStruct(myName : "Bhavadharani" , myAge : 21, course : "computerscience", city : "coimbatore")
-let newCity2 = newCity
 
-
-newCity2.city = "madurai"
-
-print("\(newCity.myAge)")
-       print("\(newCity2.city)")
-
-struct NewStruct1{
-    let myName = "Bhavadharani"
-    let myAge = 21
-    let course = "computer"
-    var city = " chennai"
+func checkForNonNil(value: newStruct) {
+    guard let course1 = value.course else {
+        print("Value doesn't esists")
+        return
+    }
+    print("Value of course:\(course1)")
 }
-struct NewStruct2
 
+let newCity1 = newStruct(myName : "Bhavadharani2" , myAge : 22, course: "CSE",  city : "coimbatore1")
+let newCity = newStruct(myName : "Bhavadharani" , myAge : 21, city : "coimbatore")
+//print("\(newCity.myName)")
+//print("\(newCity.myAge)")
+checkForNonNil(value: newCity1)
+
+if
+    let newVariable = newCity.course
 {
-    let myName = "darani"
-    let myAge = 20
-    let course = "maths"
-    var city = "coimbatore"
+    print("value exists,\(newVariable)")
+}
+else
+{
+print("Nil")
 }
 
+    
 
-var access2 = NewStruct1()
-var access3 = access2
-var access1 = NewStruct2()
-var access4 = access3
 
-access2.city = "erode"
-access1.city = "trichy"
-print(access3.city)
-print(access4.city)
+
+print("\(newCity.city)")
+//if let course1 = newCity1.course else
+//{
+//    print("Nil")
+//}
+//
+//    print("The course is : \(course1)")
+//}
+
+    
+//print("\(newCity.city)")
+//
+//
+//let newCity2 = newCity
+//
+//
+//newCity2.city = "madurai"
+//
+//print("\(newCity.myAge)")
+//       print("\(newCity2.city)")
+//
+//struct NewStruct1{
+//    let myName = "Bhavadharani"
+//    let myAge = 21
+//    let course = "computer"
+//    var city = " chennai"
+//}
+//struct NewStruct2
+//
+//{
+//    let myName = "darani"
+//    let myAge = 20
+//    let course = "maths"
+//    var city = "coimbatore"
+//}
+//
+//
+//var access2 = NewStruct1()
+//var access3 = access2
+//var access1 = NewStruct2()
+//var access4 = access3
+//
+//access2.city = "erode"
+//access1.city = "trichy"
+//print(access3.city)
+//print(access4.city)
---
+++ Basics-1.playground/Resources/Data Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//memory size
+print(MemoryLayout<Int>.size)
+print(MemoryLayout<Float>.size)
+print(MemoryLayout<Character>.size)
+print(MemoryLayout<Double>.size)
+print(MemoryLayout<String>.size)
+print(MemoryLayout<Bool>.size)
+
+
+//
+var intVar: Int = 9
+var float: Float = 23.45
+var string: String = "Apple"
+var character: Character = "R"
+var double: Double = 23.45678
+ print("\(intVar),\(float),\(string),\(character),\(double)")
+
+
